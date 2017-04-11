443095edb923895bd6993c886ae64a3fe629f2fe
IzeroCs <izero.cs@gmail.com>
2017-04-11 21:04:11 +0700

    Fix cookie_path

--- assets/config/app.php
+++ assets/config/app.php
@@ -68,7 +68,7 @@
                 'use_token'   => true,
                 'key_name'    => '_cfsr_token',
                 'time_live'   => 60000,
-                'path_cookie' => '/${app.directory_absolute}/',
+                'path_cookie' => '/${app.directory_absolute_http}/',
 
                 'validate_post' => true,
                 'validate_get'  => true
