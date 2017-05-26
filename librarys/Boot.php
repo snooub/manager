7796c123c9b94d1390461f489c4d8370f45207e0
IzeroCs <izero.cs@gmail.com>
2017-05-27 02:14:57 +0700

    Fix ob_start

--- librarys/Boot.php
+++ librarys/Boot.php
@@ -103,7 +103,7 @@
 
         public function obBufferStart()
         {
-            if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
+            if (isset($_SERVER['HTTP_ACCEPT_ENCODING']) && substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
                 @ob_start('ob_gzhandler');
             else
                 @ob_start();
