9f28031b118125e717f04ca0d32890f6aa1d41bb
IzeroCs <izero.cs@gmail.com>
2017-05-29 01:17:06 +0700

    Update about

--- assets/config/about.php
+++ assets/config/about.php
@@ -1,17 +1,18 @@
 <?php
 
 	return [
-	    'name'      => 'Manager',
-		'author'    => 'Izero.Cs',
-		'version'   => '3.5',
-		'email'     => 'Izero.Cs@gmail.com',
-		'fb_link'   => 'https://facebook.com/Izero.Cs',
-		'fb_title'  => 'fb.com/Izero.Cs',
-		'git_link'  => 'https://github.com/IzeroCs/Manager',
-		'git_title' => 'IzeroCs/Manager',
-		'phone'     => '+841685929323',
-		'create_at' => '1434468025',
-		'update_at' => time()
+	    'name'       => 'Manager',
+		'author'     => 'Izero.Cs',
+		'version'    => '3.5 beta',
+		'email'      => 'Izero.Cs@gmail.com',
+		'fb_link'    => 'https://facebook.com/Izero.Cs',
+		'fb_title'   => 'fb.com/Izero.Cs',
+		'git_link'   => 'https://github.com/IzeroCs/Manager',
+		'git_title'  => 'IzeroCs/Manager',
+		'phone'      => '+841685929323',
+		'create_at'  => '1434468025',
+		'upgrade_at' => 0,
+        'check_at'   => 0
 	];
 
 ?>
