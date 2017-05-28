d73aabdff57ddb1c54cecfec6ec2bce37ca8ca33
IzeroCs <izero.cs@gmail.com>
2017-05-29 01:16:32 +0700

    Add more action in about

---
+++ app/validate_app.php
@@ -0,0 +1,16 @@
+<?php
+
+    define('LOADED', 1);
+    require_once('global.php');
+
+    $title  = lng('app.validate_app.title_page');
+    require_once(ROOT . 'incfiles' . SP . 'header.php');
+?>
+
+    <ul class="alert">
+        <li class="info">
+            <span><?php echo lng('home.alert.features_is_construct'); ?></span>
+        </li>
+    </ul>
+
+<?php require_once(ROOT . 'incfiles' . SP . 'footer.php'); ?>
