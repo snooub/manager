7e15623f659aff5da6bd284c1859fc3a40ff630d
IzeroCs <izero.cs@gmail.com>
2017-05-29 01:14:36 +0700

    Change position lang about and more info

---
+++ assets/language/vi/app/about.php
@@ -0,0 +1,29 @@
+<?php
+
+    return [
+        'title_page' => 'Thông tin ứng dụng',
+
+		'info' => [
+			'label' => [
+				'author'     => 'Tác giả:',
+				'version'    => 'Phiên bản:',
+				'email'      => 'Email:',
+				'github'     => 'Github:',
+				'facebook'   => 'Facebook:',
+				'phone'      => 'Số điện thoại:',
+				'create_at'  => 'Tạo ngày:',
+                'upgrade_at' => 'Nâng cấp ngày:',
+                'check_at'   => 'Kiểm tra ngày:'
+			]
+		],
+
+        'menu_action' => [
+            'about'        => 'Thông tin ứng dụng',
+            'check_update' => 'Kiểm tra cập nhật',
+            'validate_app' => 'Kiểm tra ứng dụng',
+            'help'         => 'Trợ giúp',
+            'feedback'     => 'Góp ý',
+        ]
+    ];
+
+?>
