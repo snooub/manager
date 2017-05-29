649fc1b4290f486fcea5e20ed38912dab3750879
IzeroCs <izero.cs@gmail.com>
2017-05-29 09:46:39 +0700

    Update check_update

--- assets/language/vi/app/check_update.php
+++ assets/language/vi/app/check_update.php
@@ -7,7 +7,8 @@
             'label' => [
                 'last_check_update' => 'Thời gian kiểm tra cuối:',
                 'last_upgrade'      => 'Thời gian nâng cấp cuối:',
-                'version_current'   => 'Phiên bản hiện tại:'
+                'version_current'   => 'Phiên bản hiện tại:',
+                'server_check'      => 'Địa chỉ máy chủ kiểm tra ({$index}):'
             ],
 
             'value' => [
@@ -23,16 +24,21 @@
         ],
 
         'alert' => [
-            'check_update_failed'          => 'Kiểm tra cập nhật thất bại',
-            'address_not_found'            => 'Địa chỉ <strong>{$url}</strong> không tồn tại',
-            'file_not_found'               => 'Địa chỉ tập tin <strong>{$url}</strong> không tồn tại trên trang này',
-            'auto_redirect_url_failed'     => 'Địa chỉ <strong>{$url}</strong> chuyển hướng thất bại',
-            'connect_url_failed'           => 'Kết nối tới địa chỉ <strong>{$url}</strong> thất bại',
-            'error_json_data'              => 'Lỗi dữ liệu địa chỉ <strong>{$url}</strong> có vấn đề',
-            'error_json_data_not_validate' => 'Lỗi dữ liệu địa chỉ <strong>{$url}</strong> không hợp lệ',
-            'error_unknown'                => 'Không rõ lỗi cho địa chỉ <strong>{$url}</strong>',
-            'version_is_old'               => 'Phiên bản <strong>{$version_current}</strong> hiện tại đã cũ, đã có phiên bản mới <strong>{$version_update}</strong> bạn hãy nhấn tải về để cập nhật',
-            'version_is_latest'            => 'Phiên bản <strong>{$version_current}</strong> hiện tại là mới nhất'
+            'not_server_check'                          => 'Không có địa chỉ máy chủ nào để kiểm tra',
+            'address_not_found'                         => 'Địa chỉ <strong>{$url}</strong> không tồn tại',
+            'file_not_found'                            => 'Địa chỉ tập tin <strong>{$url}</strong> không tồn tại trên trang này',
+            'auto_redirect_url_failed'                  => 'Địa chỉ <strong>{$url}</strong> chuyển hướng thất bại',
+            'connect_url_failed'                        => 'Kết nối tới địa chỉ <strong>{$url}</strong> thất bại',
+            'error_json_data'                           => 'Lỗi dữ liệu địa chỉ <strong>{$url}</strong> có vấn đề',
+            'error_json_data_not_validate'              => 'Lỗi dữ liệu địa chỉ <strong>{$url}</strong> không hợp lệ',
+            'error_not_found_list_version_in_server'    => 'Không tìm thấy danh sách phiên bản trên địa chỉ <strong>{$url}</strong>',
+            'error_not_found_parameter_guest'           => 'Lỗi địa chỉ truy vấn sai tới địa chỉ <strong>{$url}</strong>',
+            'error_version_guest_not_validate'          => 'Phiên bản của bạn truy vấn tới địa chỉ <strong>{$url}</strong> không hợp lệ',
+            'error_version_server_not_validate'         => 'Phiên bản của địa chỉ <strong>{$url}</strong> không hợp lệ',
+            'error_not_found_version_current_in_server' => 'Lỗi phiên bản hiện tại không tồn tại trên địa chỉ <strong>{$url}</strong>',
+            'error_unknown'                             => 'Không rõ lỗi cho địa chỉ <strong>{$url}</strong>',
+            'version_is_old'                            => 'Phiên bản <strong>{$version_current}</strong> hiện tại đã cũ, đã có phiên bản mới <strong>{$version_update}</strong> bạn hãy nhấn tải về để cập nhật',
+            'version_is_latest'                         => 'Phiên bản <strong>{$version_current}</strong> hiện tại là mới nhất',
         ]
     ];
 
--- librarys/App/AppUpdate.php
+++ librarys/App/AppUpdate.php
@@ -3,7 +3,7 @@
     namespace Librarys\App;
 
     use Librarys\File\FileInfo;
-    use Librarys\App\AppURLCurl;
+    use Librarys\File\FileCurl;
     use Librarys\App\AppAboutConfig;
 
     final class AppUpdate
@@ -11,9 +11,9 @@
 
         private $servers = [
             'localhost',
-//            'izerocs.mobi',
-//            'izerocs.net',
-//            'izerocs.ga'
+            // 'izerocs.mobi',
+            // 'izerocs.net',
+            // 'izerocs.ga'
         ];
 
         private $aboutConfig;
@@ -23,24 +23,36 @@
         private $jsonArray;
         private $updateStatus;
 
-        const ERROR_NONE                   = 0;
-        const ERROR_JSON_DATA              = 1;
-        const ERROR_JSON_DATA_NOT_VALIDATE = 2;
+        const PARAMETER_VERSION_GUEST_URL = 'version_guest';
+
+        const ARRAY_KEY_URL          = 'url';
+        const ARRAY_KEY_ERROR_INT    = 'error_int';
+        const ARRAY_KEY_HTTP_CODE    = 'http_code';
+        const ARRAY_KEY_ERROR_CHECK  = 'error_check';
+        const ARRAY_KEY_ERROR_SERVER = 'error_server';
+
+        const ARRAY_DATA_KEY_VERSION         = 'version';
+        const ARRAY_DATA_KEY_IS_BETA         = 'is_beta';
+        const ARRAY_DATA_KEY_CHANGELOG       = 'changelog';
+        const ARRAY_DATA_KEY_BUILD_LAST      = 'build_last';
+        const ARRAY_DATA_KEY_COMPRESS_METHOD = 'compress_method';
+        const ARRAY_DATA_KEY_DATA_UPDATE     = 'data_update';
+        const ARRAY_DATA_KEY_ERROR_INT       = 'error_int';
 
         const RESULT_NONE              = 0;
         const RESULT_VERSION_IS_LATEST = 1;
         const RESULT_VERSION_IS_OLD    = 2;
 
-        const ARRAY_KEY_URL          = 'url';
-        const ARRAY_KEY_ERROR_INT    = 'error_int';
-        const ARRAY_KEY_HTTP_CODE    = 'http_code';
-        const ARRAY_KEY_ERROR_UPDATE = 'error_update';
+        const ERROR_CHECK_NONE                   = 0;
+        const ERROR_CHECK_JSON_DATA              = 1;
+        const ERROR_CHECK_JSON_DATA_NOT_VALIDATE = 2;
 
-        const ARRAY_DATA_KEY_VERSION         = 'version';
-        const ARRAY_DATA_KEY_CHANGE_MSG      = 'change_msg';
-        const ARRAY_DATA_KEY_BUILD_LAST      = 'build_last';
-        const ARRAY_DATA_KEY_COMPRESS_METHOD = 'compress_method';
-        const ARRAY_DATA_KEY_DATA_UPDATE     = 'data_update';
+        const ERROR_SERVER_NONE                                = 0;
+        const ERROR_SERVER_NOT_FOUND_LIST_VERSION_IN_SERVER    = 1;
+        const ERROR_SERVER_NOT_FOUND_PARAMETER_VERSION_GUEST   = 2;
+        const ERROR_SERVER_VERSION_GUEST_NOT_VALIDATE          = 3;
+        const ERROR_SERVER_VERSION_SERVER_NOT_VALIDATE         = 4;
+        const ERROR_SERVER_NOT_FOUND_VERSION_CURRENT_IN_SERVER = 5;
 
         public function __construct(AppAboutConfig $about)
         {
@@ -55,8 +67,9 @@
             $countSuccess = count($this->servers);
 
             foreach ($this->servers AS $server) {
-                $curl        = new AppURLCurl($server . '/' . $this->path);
-                $errorUpdate = self::ERROR_NONE;
+                $curl        = new FileCurl($server . '/' . $this->path . '?' . self::PARAMETER_VERSION_GUEST_URL . '=' . $this->aboutConfig->get('version'));
+                $errorCheck  = self::ERROR_CHECK_NONE;
+                $errorServer = self::ERROR_SERVER_NONE;
 
                 if ($curl->curl() != false) {
                     $bufferLength = $curl->getBufferLength();
@@ -65,13 +78,15 @@
                     $jsonValidate = self::validateJsonData($jsonData);
 
                     if ($jsonData === false) {
-                        $errorUpdate = self::ERROR_JSON_DATA;
+                        $errorCheck = self::ERROR_CHECK_JSON_DATA;
                     } else if ($jsonValidate === false) {
-                        $errorUpdate = self::ERROR_JSON_DATA_NOT_VALIDATE;
+                        $errorCheck = self::ERROR_CHECK_JSON_DATA_NOT_VALIDATE;
                     } else {
                         $this->jsonArray = $jsonData;
 
-                        if ($this->checkDataUpdate() != false)
+                        if ($this->hasJsonArrayKey(self::ARRAY_DATA_KEY_ERROR_INT))
+                            $errorServer = $this->getJsonArrayValue(self::ARRAY_DATA_KEY_ERROR_INT);
+                        else if ($this->checkDataUpdate() != false)
                             return true;
                     }
                 }
@@ -81,7 +96,8 @@
                     self::ARRAY_KEY_URL          => $curl->getURL(),
                     self::ARRAY_KEY_ERROR_INT    => $curl->getErrorInt(),
                     self::ARRAY_KEY_HTTP_CODE    => $curl->getHttpCode(),
-                    self::ARRAY_KEY_ERROR_UPDATE => $errorUpdate
+                    self::ARRAY_KEY_ERROR_CHECK  => $errorCheck,
+                    self::ARRAY_KEY_ERROR_SERVER => $errorServer
                 ];
             }
 
@@ -112,6 +128,11 @@
             return true;
         }
 
+        public function getServers()
+        {
+            return $this->servers;
+        }
+
         public function getServerErrors()
         {
             return $this->serverErrors;
@@ -131,6 +152,17 @@
                 return $this->jsonArray[$key];
 
             return null;
+        }
+
+        public function hasJsonArrayKey($key)
+        {
+            if (is_array($this->jsonArray) == false)
+                return false;
+
+            if (isset($this->jsonArray[$key]))
+                return true;
+
+            return false;
         }
 
         public function getVersionUpdate()
@@ -163,10 +195,13 @@
             if ($jsonArray == null || is_array($jsonArray) == false)
                 return false;
 
+            if (isset($jsonArray[self::ARRAY_DATA_KEY_ERROR_INT]))
+                return true;
+
             $keyHas      = array();
             $keyValidate = [
                 self::ARRAY_DATA_KEY_VERSION,
-                self::ARRAY_DATA_KEY_CHANGE_MSG,
+                self::ARRAY_DATA_KEY_CHANGELOG,
                 self::ARRAY_DATA_KEY_BUILD_LAST,
                 self::ARRAY_DATA_KEY_COMPRESS_METHOD,
                 self::ARRAY_DATA_KEY_DATA_UPDATE
@@ -190,9 +225,9 @@
             return false;
         }
 
-        public static function validateVersionValue($version)
+        public static function validateVersionValue($version, &$matches = null)
         {
-            return preg_match('/^\d\.\d\.\d[[:space:]]*(?:.*?)$/i', $version);
+            return preg_match('/^(\d)+\.(\d)+\.?(\d)?$/i', $version, $matches);
         }
 
     }
