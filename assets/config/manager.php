02a5f0d4a2d194ec3c5443c701bd1e70be2e8c83
IzeroCs <izero.cs@gmail.com>
2017-06-02 00:09:49 +0700

    Update config

--- assets/config/manager.php
+++ assets/config/manager.php
@@ -5,64 +5,63 @@
 
     return [
         'auto_redirect' => [
-            'file_chmod'  => true,
+            'file_chmod' => true,
             'file_rename' => true,
-
             'create_directory' => true,
-            'create_file'      => true,
-
+            'create_file' => true,
             'create_database' => true,
-            'rename_database' => true
+            'rename_database' => true,
         ],
 
         'enable_disable' => [
-            'button_save_on_javascript'       => true,
-            'auto_focus_input_last'           => true,
-            'count_checkbox_file_javascript'  => true,
-            'count_checkbox_mysql_javascript' => true
+            'button_save_on_javascript' => true,
+            'auto_focus_input_last' => true,
+            'count_checkbox_file_javascript' => true,
+            'count_checkbox_mysql_javascript' => true,
         ],
 
         'login' => [
-            'enable_forgot_password'   => false,
+            'enable_forgot_password' => false,
             'enable_lock_count_failed' => false,
-            'max_lock_count'           => 5,
-            'time_lock'                => 180,
-            'time_login'               => 86400 * 7
+            'max_lock_count' => 5,
+            'time_lock' => 180,
+            'time_login' => 604800,
         ],
 
         'paging' => [
-            'file_edit_text'      => 30,
+            'file_edit_text' => 50,
             'file_edit_text_line' => 20,
-            'file_home_list'      => 10,
-            'file_view_zip'       => 0,
-
-            'mysql_list_data' => 5
+            'file_home_list' => 50,
+            'file_view_zip' => 0,
+            'mysql_list_data' => 5,
         ],
 
         'cache' => [
             'css' => [
-                'enable'   => true,
-                'minify'   => true,
-                'lifetime' => 31536000
+                'enable' => true,
+                'minify' => true,
+                'lifetime' => 31536000,
             ],
 
             'js' => [
-                'enable'   => true,
-                'minify'   => true,
-                'lifetime' => 31536000
-            ]
+                'enable' => true,
+                'minify' => true,
+                'lifetime' => 31536000,
+            ],
+
         ],
 
         'tmp' => [
             'lifetime' => 180,
-            'limit'    => 20
+            'limit' => 20,
         ],
 
         'theme' => [
-            'directory' => 'default'
+            'directory' => 'default',
         ],
 
-        'language' => 'vi'
+        'language' => 'vi',
+        'http_host' => 'http://izerocs.mobi/Manager',
     ];
 
 ?>
