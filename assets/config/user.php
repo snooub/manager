7590c599d90accc6dbf56f9eab2966e1f85ab79e
IzeroCs <izero.cs@gmail.com>
2017-06-02 00:31:10 +0700

    Update

--- assets/config/user.php
+++ assets/config/user.php
@@ -4,17 +4,17 @@
         exit;
 
     return [
-        'cfcd208495d565ef66e7dff9f98764da' => [
+        '766ffe498265015aadc95a41b3d5a9c0' => [
             'username' => 'IzeroCs',
-            'email' => 'izerocs@gmail.com',
-            'password' => '$2y$10$egdHPfKjOJGuvp6YHCzKTOeukYWhNw4p93HwGpYRGWSkhKYWaOEfe',
+            'email' => 'izero.cs@gmail.com',
+            'password' => '$2y$10$O5zmOHtgTsL969e5qhGrVeiZJlP1evsVZYeWrTArDLmWJWp911YKa',
             'position' => 8,
-            'create_at' => 0,
-            'modify_at' => 1496288034,
+            'create_at' => 1496336631,
+            'modify_at' => 1496336853,
+            'login_at' => 1496336856,
             'band_at' => 0,
             'band_of' => '',
-            'login_at' => 1496314900,
-        ],
+        ]
 
     ];
 
