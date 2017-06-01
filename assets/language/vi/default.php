43f05fa4af0ceeaaecf116a233c51eb4a6ca3ce6
IzeroCs <izero.cs@gmail.com>
2017-06-02 00:20:25 +0700

    Update lang

--- assets/language/vi/default.php
+++ assets/language/vi/default.php
@@ -2,12 +2,16 @@
 
     return [
         'global' => [
-            'app_is_install_root'     => 'Bạn đang cài đặt ứng dụng trên thư mục gốc, hãy cài đặt vào một thư mục con',
-            'host_is_not_premission'  => 'Có vẻ host bạn cài ứng dụng không thể thực thi được, bạn vui lòng kiểm tra lại',
-            'config_app_not_found'    => 'Cấu hình cho ứng dụng không tồn tại, bạn hãy xóa bỏ ứng dụng và cài lại thử xem',
-            'unknown_error_app'       => 'Không thể xác định lỗi, bạn hãy kiểm tra lại, hoặc cài lại ứng dụng',
-            'cfsr_token_not_validate' => 'CFSR Token không hợp lệ',
-            'ip_not_validate'         => 'IP <strong>{$ip}</strong> của bạn không hợp lệ'
+            'app_is_install_root'       => 'Bạn đang cài đặt ứng dụng trên thư mục gốc, hãy cài đặt vào một thư mục con',
+            'host_is_not_premission'    => 'Có vẻ host bạn cài ứng dụng không thể thực thi được, bạn vui lòng kiểm tra lại',
+            'config_app_not_found'      => 'Cấu hình cho ứng dụng không tồn tại, bạn hãy xóa bỏ ứng dụng và cài lại thử xem',
+            'unknown_error_app'         => 'Không thể xác định lỗi, bạn hãy kiểm tra lại, hoặc cài lại ứng dụng',
+            'cfsr_token_not_validate'   => 'CFSR Token không hợp lệ',
+            'ip_not_validate'           => 'IP <strong>{$ip}</strong> của bạn không hợp lệ',
+            'change_config_failed'      => 'Thay đổi cấu hình phù hợp cho host của bạn thất bại hãy tải lại trang',
+            'change_config_success'     => 'Thay đổi cấu hình phù hợp cho host của bạn thành công hãy tải lại trang',
+            'create_first_user_success' => 'Tạo người dùng đầu tiên thành công. Tài khoản <strong>{$username}</strong>, mật khẩu <strong>{$password}</strong>, hãy thay đổi thông tin ngay khi đăng nhập thành công',
+            'create_first_user_failed'  => 'Tạo người dùng đầu tiên thất bại, hãy tải lại trang để tạo lại tự động'
         ],
 
         'resource' => [
--- assets/language/vi/home.php
+++ assets/language/vi/home.php
@@ -11,11 +11,12 @@
         'checkbox_all_entry' => 'Chọn tất cả',
 
         'alert' => [
-            'features_is_construct'  => 'Tính năng đang trong quá trình xây dựng',
-            'path_not_exists'        => 'Đường dẫn không tồn tại',
-            'path_not_receiver_list' => 'Không thể lấy danh sách tập tin',
-            'path_not_permission'    => 'Đường dẫn <strong>"{$path}"</strong> bạn truy cập thuộc của ứng dụng nên không thể truy cập',
-            'path_not_is_file'       => 'Đường dẫn này không phải một tập tin'
+            'features_is_construct'          => 'Tính năng đang trong quá trình xây dựng',
+            'path_not_exists'                => 'Đường dẫn không tồn tại',
+            'path_not_receiver_list'         => 'Không thể lấy danh sách tập tin',
+            'path_not_permission'            => 'Đường dẫn <strong>"{$path}"</strong> bạn truy cập thuộc của ứng dụng nên không thể truy cập',
+            'path_not_is_file'               => 'Đường dẫn này không phải một tập tin',
+            'password_user_is_equal_default' => 'Bạn chưa thay đổi mật khẩu mặc định, điều này rất nguy hiểm hãy thay đổi ngay'
         ],
 
         'action_multi' => [
