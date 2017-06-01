a0b77e2f4937284b0c7a1fb3b8ae80c9e4412c38
IzeroCs <izero.cs@gmail.com>
2017-06-02 00:27:07 +0700

    Fix write config mysql

--- mysql/disconnect.php
+++ mysql/disconnect.php
@@ -4,13 +4,8 @@
     require_once('global.php');
 
     if ($appMysqlConfig->set('mysql_is_connect', false)) {
-        $appMysqlConfigWrite = new Librarys\App\Mysql\AppMysqlConfigWrite($appMysqlConfig);
-        $appMysqlConfigWrite->setSpacing('    ');
-
-        if ($appMysqlConfig->write()) {
-            $boot->sleepFixHeaderRedirectUrl();
+        if ($appMysqlConfig->write())
             $appAlert->success(lng('mysql.home.alert.disconnect_success'), ALERT_MYSQL_HOME, 'index.php');
-        }
     }
 
     $appAlert->success(lng('mysql.home.alert.disconnect_failed'), ALERT_MYSQL_HOME, 'list_database.php');
