8673e19375acf5a3d264d2ee94831b4fcf3b57f7
IzeroCs <izero.cs@gmail.com>
2017-10-12 10:38:01 +0700

    Fix ajax upload

--- assets/javascript/dev/ajax.js
+++ assets/javascript/dev/ajax.js
@@ -111,7 +111,7 @@ var Ajax = {
                                     dataSend.append(input.name, value);
                                 }
                             } else if (input.type === "file") {
-                                var files = input.files;
+                                var files = input[input.name];
 
                                 if (files && files.length && files.length > 0) {
                                     for (var j = 0; j < files.length; ++j)
